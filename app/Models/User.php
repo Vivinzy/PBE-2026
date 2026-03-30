9d80be518b72ac74360fab46ede72ef30f8f40b2
vivi <vitorianunespera@gmail.com>
2026-03-30 16:49:04 -0300

    30.03.2026

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,25 +11,19 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Leek\FilamentDiceBear\Concerns\HasDiceBearAvatar;
 use Leek\FilamentDiceBear\Enums\DiceBearStyle;
+use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable implements FilamentUser, HasAvatar
+class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasDiceBearAvatar;
-
-    public function canAccessPanel(Panel $panel): bool
-    {
-        return true;
-    }
-
-    public function dicebearAvatarStyle(): DiceBearStyle
-    {
-        return DiceBearStyle::Adventurer;
-    }
-
+    use HasFactory, Notifiable, HasRoles;
+/**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
     protected function casts(): array
     {
         return [
