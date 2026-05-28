a40ec85d4bed8936adc6605b87f3974b63713045
vivi <vitorianunespera@gmail.com>
2026-05-27 22:06:13 -0300

    Somativa 03 - incompleta

--- SOMATIVA 03/safe/app/Filament/Resources/AlunoResource/Pages/CreateAluno.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Filament\Resources\AlunoResource\Pages;
-use App\Filament\Resources\AlunoResource;
-use Filament\Resources\Pages\CreateRecord;
-class CreateAluno extends CreateRecord {
-    protected static string $resource = AlunoResource::class;
-    protected function getRedirectUrl(): string { return $this->getResource()::getUrl('index'); }
-}
--- SOMATIVA 03/safe/app/Http/Controllers/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Log;
-
-abstract class Controller
-{
-    Log::info('Notificação enviada para responsável: saída do aluno autorizada.');
-}
